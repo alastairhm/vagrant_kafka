60b91131dcb775079cf236289889044c4fd3a480
Alastair Montgomery <amontgomery@williamhill.co.uk>
2016-12-12 14:31:56 +0000

    auto start up add Kafka bin to path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-wget http://apache.mirror.anlx.net/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz -O /home/ubuntu/kafka.tgz
+wget -q http://apache.mirror.anlx.net/kafka/0.10.1.0/kafka_2.11-0.10.1.0.tgz -O /home/ubuntu/kafka.tgz
 mkdir /home/ubuntu/kafka
 tar -xf kafka.tgz -C /home/ubuntu/kafka
 ln -s /vagrant/tmux.conf /home/ubuntu/.tmux.conf
 ln -s /home/ubuntu/kafka/kafka_2.11-0.10.1.0 /home/ubuntu/kafka/kafka
+echo -e "\nexport PATH=\$PATH:/vagrant:/home/ubuntu/kafka/kafka/bin\n" >> /home/ubuntu/.bashrc
+chown -R ubuntu. /home/ubuntu/kafka
+rm -f /home/ubuntu/kafka.tgz
+/vagrant/start.sh
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export KAFKA_HOME=/home/ubuntu/kafka/kafka
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties > zookeeper.log &
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > kafka.log &
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties > /home/ubuntu/zookeeper.log &
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > /home/ubuntu/kafka.log &
