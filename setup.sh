0e2870b71116a50288ad2222ea8100d9e67afcd9
Alastair Montgomery <amontgomery@williamhill.co.uk>
2020-12-18 14:26:23 +0000

    End of year push

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ tar -xf kafka.tgz -C /home/ubuntu/kafka
 ln -s /vagrant/tmux.conf /home/ubuntu/.tmux.conf
 ln -s /home/ubuntu/kafka/kafka_2.11-0.10.1.0 /home/ubuntu/kafka/kafka
 echo -e "\nexport PATH=\$PATH:/vagrant:/home/ubuntu/kafka/kafka/bin\n" >> /home/ubuntu/.bashrc
+echo -e "export KAFKA_HOME=/home/ubuntu/kafka/kafka\n" >> /home/ubuntu/.bashrc
 chown -R ubuntu. /home/ubuntu/kafka
 rm -f /home/ubuntu/kafka.tgz
 /vagrant/start.sh
